9658431fe67f6af563960f570edb1f2e93e241bd
BJTMastermind <bjtmastermind@gmail.com>
2023-02-15 02:53:54 -0500

    Added unknown block placeholder from Bedrock Edition

--- src/main/java/me/bjtmastermind/datadrivenblocks/DataDrivenBlocksMod.java
+++ src/main/java/me/bjtmastermind/datadrivenblocks/DataDrivenBlocksMod.java
@@ -4,12 +4,14 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import me.bjtmastermind.datadrivenblocks.block.ModBlocks;
+
 public class DataDrivenBlocksMod implements ModInitializer {
     public static final String MODID = "datadrivenblocks";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello Fabric world!");
+        ModBlocks.registerModBlocks();
     }
 }
---
+++ src/main/java/me/bjtmastermind/datadrivenblocks/block/ModBlocks.java
@@ -0,0 +1,33 @@
+package me.bjtmastermind.datadrivenblocks.block;
+
+import me.bjtmastermind.datadrivenblocks.DataDrivenBlocksMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static final Block UNKNOWN_BLOCK = registerBlock("unknown",
+        new UnknownBlock(FabricBlockSettings.of(Material.STONE).breakInstantly().resistance(0f).hardness(0f).mapColor(MapColor.DIRT_BROWN)));
+
+    public static void registerModBlocks() {
+        DataDrivenBlocksMod.LOGGER.info("Registering mod blocks for "+DataDrivenBlocksMod.MODID);
+    }
+
+    private static Block registerBlock(String id, Block block) {
+        registerBlockItem(id, block);
+        return Registry.register(Registries.BLOCK, new Identifier(DataDrivenBlocksMod.MODID, id), block);
+    }
+
+    private static Item registerBlockItem(String id, Block block) {
+        Item item = Registry.register(Registries.ITEM, new Identifier(DataDrivenBlocksMod.MODID, id),
+            new BlockItem(block, new FabricItemSettings()));
+        return item;
+    }
+}
