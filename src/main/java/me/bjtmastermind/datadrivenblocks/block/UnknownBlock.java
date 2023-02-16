77d5102854394e410a16fc67d9a46cd446d19f53
BJTMastermind <bjtmastermind@gmail.com>
2023-02-15 21:30:37 -0500

    Made Unknown block placeable by players - Must have been a bug on bedrock where it was acting like that for me.

--- src/main/java/me/bjtmastermind/datadrivenblocks/block/UnknownBlock.java
+++ src/main/java/me/bjtmastermind/datadrivenblocks/block/UnknownBlock.java
@@ -1,30 +1,10 @@
 package me.bjtmastermind.datadrivenblocks.block;
 
-import me.bjtmastermind.datadrivenblocks.DataDrivenBlocksMod;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class UnknownBlock extends Block {
 
     public UnknownBlock(Settings settings) {
         super(settings);
-    }
-
-    @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
-        if (placer instanceof PlayerEntity) {
-            world.removeBlock(pos, false);
-        }
     }
 }
